51bd68a1b6c8fdc4465c7202e39144c9b373dc84
Nadeem <nadeemnagarji@gmail.com>
2024-07-10 00:25:28 +0530

    added boiler plate code

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 export { app };
